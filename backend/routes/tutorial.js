04621a2ad4723e0c6f5f080012b9768954f84b02
Clemsimao <simaoclement33@gmail.com>
2025-08-20 13:40:13 +0200

    feat: Add cloudinary setup for the backend, with multer. Config files for the 2 modules and starting routes files for tutorial.
    Co-authored-by: Julien-Damlejian <Julien-Damlejian@users.noreply.github.com>
    Co-authored-by: ChloeAlberge <chloealberge@gmail.com>

---
+++ backend/routes/tutorial.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import upload from '../config/multer.js';
+import { Tutorial } from '../models/index.js';
+
+const router = express.Router();
+
+// Route POST /tutorials/:id/image
+router.post('/:id/image', upload.single('image'), async (req, res) => {
+    console.log('Route image appelée', req.params.id, req.file);
+  try {
+    const tutorialId = req.params.id;
+    const imageUrl = req.file.path; // URL Cloudinary
+
+    // Met à jour le tutoriel avec l’URL de l’image
+    await Tutorial.update(
+      { image_url: imageUrl },
+      { where: { tutorial_id: tutorialId } }
+    );
+
+    res.json({ success: true, imageUrl });
+  } catch (error) {
+    res.status(500).json({ success: false, message: 'Erreur upload image' });
+  }
+});
+
+
+export default router;
