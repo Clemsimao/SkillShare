e9b1a7c1a6e3e2b1bca8538a588da6f7f0985106
Fabien FRANCEZON <francezonfabien@gmail.com>
2025-08-21 16:01:44 +0200

    build : modal connexion extracted into components [FF]

---
+++ frontend/src/components/profile/username/page.tsx
@@ -0,0 +1,10 @@
+// Fichier usernam/page.tsx
+import Link from "next/link";
+
+// ---------- Sections réutilisables ----------
+import ProfileHeader from "@/components/profile/ProfileHeader";
+import ProfileInfoList from "@/components/profile/ProfileInfoList";
+import ProfileAbout from "@/components/profile/ProfileAbout";
+import ProfileFavorites from "@/components/profile/ProfileFavorites";
+import ProfileCounters from "@/components/profile/ProfileCounters";
+import 
