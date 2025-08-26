90f0cecad899e2f24c87dd2f3863d1a7b0888a4f
Fabien FRANCEZON <francezonfabien@gmail.com>
2025-08-26 15:40:55 +0200

    build: tuto creation page [MZ]

--- frontend/src/app/tutorials/CreateTuto/page.tsx
+++ frontend/src/app/tutorials/CreateTuto/page.tsx
@@ -5,6 +5,7 @@ import TutorialForm from "@/components/tutorials/TutorialForm";
 
 // Fonction du composant qui sert à créer un tuto
 export default function NewTutorialPage() {
+//  { Rendu principal du formulaire de création tuto }
   return (
     <main className="min-h-screen bg-base-100 px-4 py-6 flex flex-col items-center">
       <TutorialForm />
