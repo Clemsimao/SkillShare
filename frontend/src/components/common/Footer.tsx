eb7b8c0b0d1bec73ad4334886608b175fcb8df70
Fabien FRANCEZON <francezonfabien@gmail.com>
2025-08-26 15:28:59 +0200

    feat : footer's contact modal created - build : folders restructure [FF]

--- frontend/src/components/common/Footer.tsx
+++ frontend/src/components/common/Footer.tsx
@@ -87,7 +87,7 @@ export default function Footer() {
             ))}
           </div>
         </div>
-            </div>
+      </div>
       <div className="w-full flex flex-col sm:flex-row justify-between items-center text-center sm:text-left">
         {renderBranding()}
         {renderSocialLinks()}
