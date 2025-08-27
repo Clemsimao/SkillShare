dc0795960fb2862384e21fbbad673cad3941586c
Fabien FRANCEZON <francezonfabien@gmail.com>
2025-08-27 15:58:09 +0200

    feat : layout improvement [FF]

--- frontend/src/app/profile/[username]/favourites/page.tsx
+++ frontend/src/app/profile/[username]/favourites/page.tsx
@@ -12,24 +12,11 @@ export default function FavouritesPage() {
 
   return (
     <div className="min-h-screen bg-base-100 p-4 sm:p-8">
-  {/* En-tête de la page */}
-  <div className="mb-8 flex justify-center">
-    <h1 className="group group-hover:before:duration-500 group-hover:after:duration-500 after:duration-500 hover:border-green-300 hover:before:[box-shadow:_20px_20px_20px_30px_cyan] duration-500 before:duration-500 hover:duration-500 relative bg-neutral-800 h-16 w-64 border text-center p-3 text-gray-50 text-base font-bold rounded-lg overflow-hidden before:absolute before:w-12 before:h-12 before:content[''] before:right-1 before:top-1 before:z-10 before:bg-blue-500 before:rounded-full before:blur-lg after:absolute after:z-10 after:w-20 after:h-20 after:content[''] after:bg-green-300 after:right-8 after:top-3 after:rounded-full after:blur-lg uppercase">
-      MES FAVORIS
-    </h1>
-  </div>
-  <div className="item-center">
-    <Link
-      href="/"
-      className="hover:text-primary transition-colors items-center flex gap-1 justify-center"
-    >
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-      </svg>
-    </Link>
-  </div>
-
-
+      {/* En-tête de la page */}
+      <div className="mb-8 flex justify-center">
+        
+        <h1 className="group group-hover:before:duration-500 group-hover:after:duration-500 after:duration-500 hover:border-green-300 hover:before:[box-shadow:_20px_20px_20px_30px_cyan] duration-500 before:duration-500 hover:duration-500 underline-offset-2 hover:after:-right-8 hover:before:right-12 hover:before:-bottom-8 hover:before:blur origin-left hover:decoration-2 hover:text-green-300 relative bg-neutral-800 h-16 w-64 border text-left p-3 text-gray-50 text-base font-bold rounded-lg  overflow-hidden  before:absolute before:w-12 before:h-12 before:content[''] before:right-1 before:top-1 before:z-10 before:bg-blue-500 before:rounded-full before:blur-lg  after:absolute after:z-10 after:w-20 after:h-20 after:content['']  after:bg-green-300 after:right-8 after:top-3 after:rounded-full after:blur-lg uppercase">Mes Favoris</h1>
+      </div>
 
       {/* Liste des favoris */}
       <div className="max-w-3xl mx-auto">
