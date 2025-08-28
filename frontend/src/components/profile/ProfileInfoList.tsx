7b02aff3788cfb6b5fde4fea0954ae76e9f88283
Fabien FRANCEZON <francezonfabien@gmail.com>
2025-08-28 14:20:28 +0200

    style : harmonisation between pages [FF]

--- frontend/src/components/profile/ProfileInfoList.tsx
+++ frontend/src/components/profile/ProfileInfoList.tsx
@@ -6,9 +6,9 @@ export default function ProfileInfoList(props: {
     gender: string;
     city: string;
 })  {
- const { username, fullname, age, gender, city } = props;
+const { username, fullname, age, gender, city } = props;
 // Sous-composant Row pour éviter la répétition de code 
-   const Row = ({ label, value }: { label: string; value: string | number }) => (
+  const Row = ({ label, value }: { label: string; value: string | number }) => (
     // Ce div est stylisé comme un bouton pour des raisons de design.
     <div className="btn btn-sm btn-outline w-full justify-between">
       <span>{label}</span>
@@ -19,10 +19,10 @@ export default function ProfileInfoList(props: {
 // Rendu final du composant.
   return (
     // Section principale contenant les lignes d'informations
-    <section className="space-y-3 bg-base-200 rounded-lg border border-[#334155] p-5">
+    <section className="space-y-3 bg-base-200 rounded-lg p-5">
       {/* Ligne statique qui affiche le pseudo */}
       <div className="btn w-full cursor-default">
-         <span className="p-10 opacity-80">@{username}</span>
+        <span className="p-10 opacity-80">@{username}</span>
       </div>
 
       {/* Chaque ligne d'information utilise le sous-composant Row */}
@@ -33,6 +33,3 @@ export default function ProfileInfoList(props: {
     </section>
   );
 }
-
-
- 
