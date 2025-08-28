718b4cdd050f89e015c84cf898a7aedda89c52f5
Fabien FRANCEZON <francezonfabien@gmail.com>
2025-08-28 16:07:01 +0200

    style : Header component update [FF]

--- frontend/src/components/profile/ProfileHeader.tsx
+++ frontend/src/components/profile/ProfileHeader.tsx
@@ -19,35 +19,37 @@ export default function ProfileHeader(props:  {
     return (
       
     <div className="card-body p-0">
-bg-gradient-to-tr from-cyan-500 via-cyan-600 to-teal-600
+
 <div
-  className="relative overflow-hidden w-full h-80 rounded-3xl cursor-pointer text-2xl font-bold bg-neutral-50 rounded-xl bg-gradient-to-tr from-cyan-500 via-cyan-600 to-teal-600"
+  className="relative overflow-hidden w-full h-80 rounded-3xl cursor-pointer text-2xl font-bold bg-gradient-to-tr from-cyan-500 via-cyan-600 to-teal-600"
 >
   <div className="z-10 absolute w-full h-full peer"></div>
   <div
-    className="absolute peer-hover:-top-20 peer-hover:-left-16 peer-hover:w-[140%] peer-hover:h-[140%] -top-33 -left-20 w-32 h-44 rounded-full bg-emerald-500 transition-all duration-500"
-  ></div>
+    className="absolute border-4 border-color-white peer-hover:-top-20 peer-hover:-left-16 peer-hover:w-[140%] peer-hover:h-[140%] -top-28 -left-16 w-32 h-44 rounded-full bg-emerald-500 transition-all duration-500"
+  >
+    
+  </div>
   <div
-    className="absolute flex text-xl text-center items-end justify-end peer-hover:right-0 peer-hover:rounded-b-none peer-hover:bottom-0 peer-hover:items-center peer-hover:justify-center peer-hover:w-full peer-hover:h-full -bottom-32 -right-16 w-36 h-44 rounded-full bg-emerald-500 transition-all duration-500"
+    className="absolute flex text-xl text-center items-end justify-end peer-hover:right-0 peer-hover:rounded-b-none peer-hover:bottom-0 peer-hover:items-center peer-hover:justify-center peer-hover:w-full peer-hover:h-full -bottom-32 -right-16 w-36 h-44 rounded-full bg-emerald-500 transition-all duration-500 opacity-0 peer-hover:opacity-100"
   >
       <div className="stats bg-transparent shadow-none mt-2 w-full">
         <div className="grid grid-cols-1 w-full">
 
           {/* --- Nombre de tutos --- */}
-          <div className="stat place-items-center p-2 text-stone-700 stat-title text-xs">
-            <div className="text-stone-700 stat-title text-xs">Tutos</div>
+          <div className="stat place-items-center p-2">
+            <div className="text-white stat-title text-xl">Tutoriels</div>
             <div className="stat-value text-lg text-white">{stats.tutorials}</div>
           </div>
 
           {/* --- Nombre d'abonnés --- */}
           <div className="stat place-items-center p-2">
-            <div className="text-white stat-title text-xs">Abonnés</div>
+            <div className="text-white stat-title text-xl">Abonnés</div>
             <div className="stat-value text-lg text-white">{stats.followers}</div>
           </div>
 
           {/* --- Nombre d'abonnements --- */}
           <div className="stat place-items-center p-2">
-            <div className="text-white stat-title text-xs">Abonnements</div>
+            <div className="text-white stat-title text-xl">Abonnements</div>
             <div className="stat-value text-lg text-white">{stats.following}</div>
           </div>
         </div>
